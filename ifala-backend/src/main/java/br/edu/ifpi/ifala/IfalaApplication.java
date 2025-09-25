c11593c0b15134aa68107c3d338d438bfdf3ee84
ReneDouglas <rene.douglas.bsi@gmail.com>
2025-09-25 12:55:44 -0300

    first commit

---
+++ ifala-backend/src/main/java/br/edu/ifpi/ifala/IfalaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.ifala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfalaApplication.class, args);
+	}
+
+}
---
+++ ifala-backend/src/test/java/br/edu/ifpi/ifala/IfalaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpi.ifala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
