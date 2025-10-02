842aa4eff841a9f8be113e7bcbbdfec8c7585893
jhonatasjgr <jhon.gomes.r@gmail.com>
2025-10-02 10:29:45 -0300

    feat: criando repository DenunciaRepository

--- apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/denuncia/DenunciaRepository.java
+++ apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/denuncia/DenunciaRepository.java
@@ -1,11 +1,29 @@
 package br.edu.ifpi.ifala.denuncia;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
 /**
- * Classe repositório para gerenciamento de entidades Denuncia. Responsável por operações de
+ * Classe repositório para gerenciamento de entidades Denuncia. Responsável por
+ * operações de
  * persistência e recuperação de denúncias no sistema.
  *
  * @author Renê Morais
+ * @author Jhonatas G Ribeiro
  */
-public class DenunciaRepository {
+public interface DenunciaRepository extends JpaRepository<Denuncia, Long>, JpaSpecificationExecutor<Denuncia> {
 
+  // Método para buscar uma denúncia pelo token de acompanhamento
+  Optional<Denuncia> findByTokenAcompanhamento(UUID tokenAcompanhamento);
+
+  // Os demais métodos de crud são herdados de JpaRepository
+  /*
+   * Os métodos de busca avançada são herdados de JpaSpecificationExecutor
+   * e gerados automaticamente pelo Spring Data JPA com base nas especificações
+   * fornecidas no service.
+   */
 }
