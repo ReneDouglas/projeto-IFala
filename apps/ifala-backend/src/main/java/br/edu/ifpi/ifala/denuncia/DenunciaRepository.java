01be5fb58d0f1e40d1afd8925559cad04dc93285
jhonatasjgr <jhon.gomes.r@gmail.com>
2025-10-14 18:04:50 -0300

    refact: removendo importação não utilizada

--- apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/denuncia/DenunciaRepository.java
+++ apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/denuncia/DenunciaRepository.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.stereotype.Repository;
 
 /**
  * Classe repositório para gerenciamento de entidades Denuncia. Responsável por
