795c81233e3b5ffe4869a73e4e55eeed34ca67ad
Guilherme25 <coradodasilva33@gmail.com>
2025-10-17 15:14:56 -0300

    feat: Adiciona e configura a dependência do Swagger (Springdoc)

--- apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/security/SecurityConfig.java
+++ apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/security/SecurityConfig.java
@@ -4,15 +4,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+  private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
   private final JwtConverter jwtConverter;
 
@@ -22,23 +29,31 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(auth -> auth
-            // endpoints públicos permitidos para todos
-            .requestMatchers("/api/v1/public/**", "/actuator/**", "/auth/primeiro-acesso", "/auth/login",
-                "/auth/logout",
-                "/auth/recuperar-senha")
-            .permitAll()
+    http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(auth -> auth
+        // endpoints públicos permitidos para todos
+        .requestMatchers("/api/v1/public/**", "/actuator/**", "/auth/primeiro-acesso",
+            "/auth/login", "/auth/logout", "/auth/recuperar-senha")
+        .permitAll()
 
-            // endpoints de admin restritos à ROLE "ADMIN"
-            // .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+        // endpoints de admin restritos à ROLE "ADMIN"
+        // .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
 
-            // qualquer outra requisição diferenteexige autenticação
-            .anyRequest().authenticated())
+        // qualquer outra requisição diferenteexige autenticação
+        .anyRequest().authenticated())
 
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        .oauth2ResourceServer(
+            oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtConverter)))
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
   }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return (web) -> web.ignoring().requestMatchers("/v3/api-docs/**", "/api-docs/**",
+        "/swagger-ui/**", "/swagger-ui.html", "/swagger-resources/**", "/webjars/**");
+  }
+
+
 }
