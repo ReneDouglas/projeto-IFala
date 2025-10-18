1e83b16b4bc516db1484363190b222962f62719e
phaolapaixao <paixaophaola@gmail.com>
2025-10-18 18:38:26 -0300

    Refatoração do LoginRequestDTO

--- apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/autenticacao/dto/LoginRequestDTO.java
+++ apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/autenticacao/dto/LoginRequestDTO.java
@@ -1,6 +1,11 @@
 package br.edu.ifpi.ifala.autenticacao.dto;
 
-public class LoginRequestDto {
+import java.io.Serializable;
+import java.util.Objects;
+
+public class LoginRequestDto implements Serializable {
+  private static final long serialVersionUID = 1L;
+
   private String email;
   private String password;
 
@@ -25,5 +30,25 @@ public class LoginRequestDto {
 
   public void setPassword(String password) {
     this.password = password;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    LoginRequestDto that = (LoginRequestDto) o;
+    return Objects.equals(email, that.email);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(email);
+  }
+
+  @Override
+  public String toString() {
+    return "LoginRequestDto{" + "email='" + email + '\'' + '}';
   }
 }
