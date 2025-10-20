89df7fba1a94d50e1e2218d161badaec9e491fe1
jhonatasjgr <jhon.gomes.r@gmail.com>
2025-10-20 17:29:56 -0300

    fix: removendo enums e colocando mais colunas na tabela de denúncia

--- scripts/postgres/init.sql
+++ scripts/postgres/init.sql
@@ -8,15 +8,15 @@ CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
 -- ENUMS
 -- ================================
 
--- Categorias de denúncia
-CREATE TYPE categorias_enum AS ENUM (
-	'celular', 'drogas', 'bullying', 'violencia', 'vandalismo', 'academico', 'outros'
-);
+-- -- Categorias de denúncia
+-- CREATE TYPE categorias_enum AS ENUM (
+-- 	'celular', 'drogas', 'bullying', 'violencia', 'vandalismo', 'academico', 'outros'
+-- );
 
--- Status da denúncia
-CREATE TYPE status_denuncia_enum AS ENUM (
-	'recebido', 'em_analise', 'aguardando', 'resolvido', 'rejeitado'
-);
+-- -- Status da denúncia
+-- CREATE TYPE status_denuncia_enum AS ENUM (
+-- 	'recebido', 'em_analise', 'aguardando', 'resolvido', 'rejeitado'
+-- );
 
 -- Tipos de notificação
 CREATE TYPE tipos_notificacao_enum AS ENUM (
@@ -36,13 +36,20 @@ CREATE TYPE perfis_enum AS ENUM (
 CREATE TABLE IF NOT EXISTS denuncias (
 	id BIGSERIAL PRIMARY KEY,
 	descricao TEXT,
-	categoria categorias_enum,
-	status status_denuncia_enum,
+	categoria VARCHAR(255), -- convertido de'categorias_enum'
+	status VARCHAR(255),    -- convertido de 'status_denuncia_enum'
 	motivo_rejeicao TEXT,
 	token_acompanhamento UUID NOT NULL UNIQUE DEFAULT gen_random_uuid(),
 	criado_em TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
 	alterado_por VARCHAR(255),
-	alterado_em TIMESTAMP WITHOUT TIME ZONE
+	alterado_em TIMESTAMP WITHOUT TIME ZONE,
+
+  deseja_se_identificar BOOLEAN DEFAULT FALSE,
+	nome_completo VARCHAR(255),
+	email VARCHAR(255),
+	grau VARCHAR(100),
+	curso VARCHAR(100),
+	turma VARCHAR(100)
 );
 
 -- Tabela acompanhamentos
