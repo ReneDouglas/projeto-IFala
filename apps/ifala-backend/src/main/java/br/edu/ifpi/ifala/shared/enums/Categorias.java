ed17f4a84bca965829c60625913584e2f783b405
jhonatasjgr <jhon.gomes.r@gmail.com>
2025-10-20 17:45:02 -0300

    refact: ajustando enum categorias

--- apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/shared/enums/Categorias.java
+++ apps/ifala-backend/src/main/java/br/edu/ifpi/ifala/shared/enums/Categorias.java
@@ -1,16 +1,20 @@
 package br.edu.ifpi.ifala.shared.enums;
 
 /**
- * Enumeração que representa as categorias possíveis para uma denúncia no sistema. Cada categoria
+ * Enumeração que representa as categorias possíveis para uma denúncia no
+ * sistema. Cada categoria
  * possui uma descrição associada para facilitar a identificação.
  *
  * @author Renê Morais
  */
 public enum Categorias {
-  CELULAR("Porte de Celular"), DROGAS("Uso ou Porte de Drogas"), BULLYING(
-      "Bullying e Assédio"), VIOLENCIA("Violência Física ou Verbal"), VANDALISMO(
-          "Vandalismo e Danos ao Patrimônio"), ACADEMICO(
-              "Questões Acadêmicas (Fraude, Plágio, etc.)"), OUTROS("Outros");
+  celular("Porte de Celular"),
+  drogas("Uso ou Porte de Drogas"),
+  bullying("Bullying e Assédio"),
+  violencia("Violência Física ou Verbal"),
+  vandalismo("Vandalismo e Danos ao Patrimônio"),
+  academico("Questões Acadêmicas (Fraude, Plágio, etc.)"),
+  outros("Outros");
 
   private String descricao;
 
