5d769597970451ea9e15b91367cc72bb253e7e36
Guilherme25 <coradodasilva33@gmail.com>
2025-10-26 10:08:17 -0300

    feat: configurar proxy Vite para comunicação com backend

--- apps/ifala-frontend/vite.config.ts
+++ apps/ifala-frontend/vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'http://ifala-backend:8080',
+        changeOrigin: true,
+      },
+    },
+  },
 });
