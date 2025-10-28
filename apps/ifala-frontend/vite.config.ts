ab761ff68034bcaa85bbaef210b4cc1e6b1e173e
Joao Andre B Santana <joaoandresantana38@gmail.com>
2025-10-28 12:23:48 -0300

    fix: corrige config proxy no vite

--- apps/ifala-frontend/vite.config.ts
+++ apps/ifala-frontend/vite.config.ts
@@ -4,27 +4,13 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-  /*
- 
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-=======
   server: {
     host: true,
     proxy: {
       '/api': {
         target: 'http://ifala-backend:8080',
->>>>>>> 77fd83da062b91bb1fbe91741f69f3468c93fcb8
         changeOrigin: true,
       },
     },
   },
-<<<<<<< HEAD
-  */
-=======
->>>>>>> 77fd83da062b91bb1fbe91741f69f3468c93fcb8
 });
